aa565b757f84174f414506a16e63419c81371d6f
Henrique Frata <henrique.frata360@gmail.com>
2022-09-30 09:55:22 -0300

    Exemplos

---
+++ aula_teste/exemplomap.py
@@ -0,0 +1,17 @@
+lista_2 = [
+    {'nome': 'João', 'idade' : 31},
+    {'nome': 'Maria', 'idade' : 37},
+    {'nome': 'José', 'idade' : 26}
+]
+
+so_nomes = map(lambda p: p['nome'], lista_2)
+print(so_nomes)
+print(list(so_nomes))
+
+list_nomes = []
+nome = ''
+for i in lista_2:
+    nome = i['nome']
+    list_nomes.append(nome)
+
+print(list_nomes)
